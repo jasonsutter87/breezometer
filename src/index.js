e6b0d37f9cfc22c018a221d49685d7fd0e33bd7f
Jason Sutter <jasonsutter87@gmail.com>
2017-09-08 14:43:02 -0500

    Implement localStorage persistence (#9)

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import BreezoReducer from './reducers';
-const store = createStore(BreezoReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+import { loadState, saveState} from './localStorage'
+const persistedState = loadState();
+const store = createStore(
+  BreezoReducer,
+  persistedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 require('dotenv').config()
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 ReactDOM.render(
   <Provider store={store}>
